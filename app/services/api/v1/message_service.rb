2eaa8d6cda32f8e06231f1daf88975cdf79aba94
Rodrigo-Miranda-0 <rodrigomc123321@gmail.com>
2022-11-30 12:48:14 -0300

    feature/notify_message

--- app/services/api/v1/message_service.rb
+++ app/services/api/v1/message_service.rb
@@ -12,12 +12,13 @@ module Api
         raise InvalidConversationError unless conversation.user1_id == @current_user.id || conversation.user2_id == @current_user.id
 
         @message = Message.create!(content: @message_params[:content], user: @current_user, conversation_id: @conversation_id)
-        notify if @message.persisted?
+        notify(conversation) if @message.persisted?
         @message
       end
 
-      def notify
-        conversation = Conversation.find(@conversation_id)
+      private
+
+      def notify(conversation)
         recipient_user = conversation.recipient_user(@current_user)
         ApplicationMailer.new_message(recipient_user, @message).deliver_now
       end
