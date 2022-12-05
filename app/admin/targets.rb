ce30ad8fd00e88a07905abaa1230b9168bf794ec
Rodrigo-Miranda-0 <rodrigomc123321@gmail.com>
2022-12-05 17:03:03 -0300

    feature/admin_index_targets

--- app/admin/targets.rb
+++ app/admin/targets.rb
@@ -1,15 +1,10 @@
-ActiveAdmin.register_page "Targets" do
-  menu priority: 2, label: proc { I18n.t("active_admin.targets") }
-
-  content title: proc { I18n.t("active_admin.targets") } do
-    # Show all registered targets
-    table_for Target.all do
-      column :id
-      column :title
-      column :user_id
-      column :topic_id
-      column :created_at
-      column :updated_at
-    end
+ActiveAdmin.register Target do
+  index do
+    column :id
+    column :title
+    column :user_id
+    column :topic_id
+    column :created_at
+    column :updated_at
   end
 end
