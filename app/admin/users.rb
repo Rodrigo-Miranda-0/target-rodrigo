d71fdf04514d75a7e204a110529d125160088c64
Rodrigo-Miranda-0 <rodrigomc123321@gmail.com>
2022-12-05 17:04:35 -0300

    feature/admin_index_users

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,15 +1,10 @@
-ActiveAdmin.register_page "Users" do
-  menu priority: 2, label: proc { I18n.t("active_admin.users") }
-
-  content title: proc { I18n.t("active_admin.users") } do
-    # Show all registered users
-    table_for User.all do
-      column :id
-      column :email
-      column :name
-      column :last_name
-      column :created_at
-      column :updated_at
-    end
+ActiveAdmin.register User do
+  index do
+    column :id
+    column :email
+    column :name
+    column :last_name
+    column :created_at
+    column :updated_at
   end
 end
