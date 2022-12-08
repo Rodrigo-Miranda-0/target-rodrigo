11080048f2056225ea5ccda3ef883fadf4b24a32
Rodrigo-Miranda-0 <rodrigomc123321@gmail.com>
2022-12-08 15:34:55 -0300

    feature/admin_filter_targets

--- app/admin/targets.rb
+++ app/admin/targets.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register Target do
-  filter :topic_id, as: :select, collection: Topic.all.map { |topic| [topic.name, topic.id] }
+  filter :topic_id, as: :select, collection: proc { Topic.all.map { |topic| [topic.name, topic.id] } }
+
   index do
     column :id
     column :title
