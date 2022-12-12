4cddce7dad6402984e63be79696d7abc90e3b32e
Rodrigo-Miranda-0 <rodrigomc123321@gmail.com>
2022-12-12 15:35:12 -0300

    feature/admin_message_history

--- app/admin/conversation.rb
+++ app/admin/conversation.rb
@@ -4,10 +4,24 @@ ActiveAdmin.register Conversation do
     id_column
     column :user1
     column :user2
-    column "Messages" do |conversation|
-      link_to "View Messages", admin_messages_path(q: { conversation_id_eq: conversation.id }), label: "Messages"
-    end
     column :created_at
     actions
+  end
+
+  show do
+    attributes_table do
+      row :user1
+      row :user2
+      row :created_at
+      row :updated_at
+    end
+
+    panel 'Messages' do
+      table_for conversation.messages do
+        column :user_id
+        column :content
+        column :created_at
+      end
+    end
   end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -4,6 +4,7 @@
 #
 #  id              :bigint           not null, primary key
 #  content         :string
+#  read            :boolean          default(FALSE)
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #  conversation_id :bigint           not null
@@ -11,14 +12,8 @@
 #
 # Indexes
 #
-#  index_messages_on_conversation_id              (conversation_id)
-#  index_messages_on_user_id                      (user_id)
-#  index_messages_on_user_id_and_conversation_id  (user_id,conversation_id) UNIQUE
-#
-# Foreign Keys
-#
-#  fk_rails_...  (conversation_id => conversations.id)
-#  fk_rails_...  (user_id => users.id)
+#  index_messages_on_conversation_id  (conversation_id)
+#  index_messages_on_user_id          (user_id)
 #
 require 'rails_helper'
 
