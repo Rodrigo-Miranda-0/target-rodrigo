8b15e5538d124cafc20334706a5e2d0135d28de5
Rodrigo-Miranda-0 <rodrigomc123321@gmail.com>
2022-12-14 11:30:34 -0300

    feature/admin_topics

--- app/admin/topic.rb
+++ app/admin/topic.rb
@@ -1,8 +1,6 @@
 ActiveAdmin.register Topic do
-  # Create topics
   permit_params :name, :image
 
-  # Show topics
   index do
     selectable_column
     id_column
@@ -13,34 +11,11 @@ ActiveAdmin.register Topic do
     actions
   end
 
-  # Edit topics
-
   form do |f|
     f.inputs "Topic Details" do
       f.input :name
       f.input :image, as: :file
     end
     f.actions
-  end
-
-  controller do
-    def create
-      Topic.create!(permitted_params[:topic])
-      flash[:notice] = I18n.t("active_admin.topic.on_create")
-      redirect_to admin_topics_path
-    end
-
-    def update
-      Topic.find(params[:id]).update!(permitted_params[:topic])
-      flash[:notice] = I18n.t("active_admin.topic.on_update")
-      redirect_to admin_topics_path
-    end
-
-    def destroy
-      topic = Topic.find(params[:id])
-      topic.destroy
-      flash[:notice] = I18n.t("active_admin.topic.on_destroy")
-      redirect_to admin_topics_path
-    end
   end
 end
