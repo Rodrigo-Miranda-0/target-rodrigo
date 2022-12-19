d5a90ff9e4142bba72a5ba6fe60e8b12c4b62ee8
Rodrigo-Miranda-0 <rodrigomc123321@gmail.com>
2022-12-19 11:49:22 -0300

    feature/create_ticket

--- app/controllers/api/v1/tickets_controller.rb
+++ app/controllers/api/v1/tickets_controller.rb
@@ -2,8 +2,7 @@ module Api
   module V1
     class TicketsController < ApiController
       def create
-        # Checks if the email is registered to an admin user
-        return render json: { error: I18n.t('api.v1.tickets.error') }, status: :bad_request if AdminUser.find_by(email: params[:email]).blank?
+        return render json: { error: I18n.t('api.v1.tickets.error') }, status: :bad_request unless admin_exists?
 
         ApplicationMailer.ticket_email(current_user, ticket_params).deliver_now
         render json: { message: I18n.t('api.v1.tickets.success') }, status: :created
@@ -13,6 +12,10 @@ module Api
 
       def ticket_params
         params.require(:ticket).permit(:email, :message)
+      end
+
+      def admin_exists?
+        AdminUser.find_by(email: params[:email]).present?
       end
     end
   end
