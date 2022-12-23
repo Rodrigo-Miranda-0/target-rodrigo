1c4d1d0b43e61ba4c33b65d81d275704fccc877f
Rodrigo-Miranda-0 <rodrigomc123321@gmail.com>
2022-12-23 11:35:31 -0300

    feature/about_users

--- app/controllers/api/v1/about_controller.rb
+++ app/controllers/api/v1/about_controller.rb
@@ -1,8 +1,10 @@
 module Api
   module V1
     class AboutController < ApiController
+      skip_before_action :authenticate_user!, only: [:index]
+
       def index
-        render json: About.first
+        render json: About.last
       end
     end
   end
--- spec/requests/api/v1/about/index_spec.rb
+++ spec/requests/api/v1/about/index_spec.rb
@@ -4,23 +4,11 @@ describe 'GET about', type: :request do
   subject { get api_v1_about_index_path, headers:, as: :json }
 
   let(:user) { create(:user) }
-  let(:headers) { user.create_new_auth_token }
   let!(:about) { create(:about) }
 
-  context 'when user is logged in' do
-    it 'returns about' do
-      subject
-      result = JSON.parse(response.body)
-      expect(result["content"]).to eq(about.content)
-    end
-  end
-
-  context 'when user is not logged in' do
-    let(:headers) { {} }
-
-    it 'returns unauthorized' do
-      subject
-      expect(response).to have_http_status(:unauthorized)
-    end
+  it 'returns about' do
+    subject
+    result = JSON.parse(response.body)
+    expect(result["content"]).to eq(about.content)
   end
 end
